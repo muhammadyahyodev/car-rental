dec286676dc8c3a54b3374209597b79fc0d4f223
muhammadyahyo <muhammadyahyodev@gmail.com>
2023-02-15 22:56:34 +0500

    static version

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ const siteBarMenuIcon = document.querySelector(".bx");
 const siteBarExit = document.querySelector(".sitebar_exit");
 const siteBarMenu = document.querySelector(".sitebar-menu");
 const siteBarMenuItem = document.querySelector(".sitebar-menu-item");
+const loginPanel = document.querySelector(".login_panel");
+const loginBtn = document.querySelector(".login_btn");
+const loginExit = document.querySelector(".registration_exit");
 
 siteBarMenuIcon.addEventListener("click", () => {
   siteBarMenuItem.classList.remove("right-[-100%]");
@@ -15,4 +18,12 @@ siteBarExit.addEventListener("click", () => {
   siteBarMenuItem.classList.add("right-[-100%]");
   siteBarMenuItem.classList.remove("right-0");
   siteBarMenu.classList.toggle("clicked");
+});
+
+loginBtn.addEventListener("click", () => {
+  loginPanel.classList.remove("hidden");
+});
+
+loginExit.addEventListener("click", () => {
+  loginPanel.classList.add("hidden");
 });
